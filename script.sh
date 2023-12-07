3e7542f9465c48a569e8469f45135b727f0a5602
marcos antonio <miciano.marcos@gmail.com>
2023-12-07 08:06:30 -0300

    2-maquina zabbix - instalacao completa

--- script.sh
+++ script.sh
@@ -5,6 +5,6 @@
    wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.4-1+ubuntu20.04_all.deb   
    dpkg -i zabbix-release_6.4-1+ubuntu20.04_all.deb
    apt update
-	apt-get install mysql-server
+   apt-get install mysql-server
    apt install zabbix-server-mysql zabbix-frontend-php zabbix-nginx-conf zabbix-sql-scripts zabbix-agent
   
